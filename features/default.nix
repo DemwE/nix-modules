395e5ba02bb286c924f5f10a3e4584a3496e9363
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-08-21 15:15:19 +0200

    feat: remove XDG desktop portal configuration

--- features/default.nix
+++ features/default.nix
@@ -20,7 +20,6 @@
     ./desktop/obs.nix
     ./desktop/dokploy.nix
     ./desktop/hyprland.nix
-    ./desktop/xdg.nix
     ./graphics/supergfxd.nix
     ./tools/ollama.nix
     ./tools/wireshark.nix
